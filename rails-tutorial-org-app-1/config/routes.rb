fb158f407037b3149256276c8c0396a0f97d8390
Ricardo Valério <jricvalerio@gmail.com>
2015-02-19 16:31:48 +0000

    in the middle of the week 1 tut and add a new app from railstutorial.com book

--- rails-tutorial-org-app-1/config/routes.rb
+++ rails-tutorial-org-app-1/config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  root to: "links#index"
-
-  resources :links do
-    member do
-      put "like",    to: "links#upvote"
-      put "dislike", to: "links#downvote"
-    end
-  end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
