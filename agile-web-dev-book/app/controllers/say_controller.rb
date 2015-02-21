d355e4e34d46968fba91fd7836024e5b955e0417
Ricardo Valério <jricvalerio@gmail.com>
2015-02-21 18:23:53 +0000

    shoshin

--- agile-web-dev-book/app/controllers/say_controller.rb
+++ agile-web-dev-book/app/controllers/say_controller.rb
@@ -1,8 +1,23 @@
 class SayController < ApplicationController
-  def hello
 
-  end
+	attr_accessor :name, :greeting, :age
 
-  def goodbay
-  end
+	def hello
+		@files = Dir.glob("*")
+	end
+
+	def goodbye
+	end
+
+	protected
+	def my_protected_method
+# can be called both in the same instance and by
+# other instances of the same class
+	end
+
+	private
+	def my_private_method
+# can be called only from within the same instance
+	end
+
 end
